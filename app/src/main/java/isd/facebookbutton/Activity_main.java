a27395aa9ce0671dbef165b028bc38a36ba5e1fd
Teodor Mytsyk <teodor.mytsyk@gmai.com>
2014-07-31 12:30:08 +0300

    Initial commit

---
+++ app/src/main/java/isd/facebookbutton/Activity_main.java
@@ -0,0 +1,223 @@
+package isd.facebookbutton;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
+import com.google.android.gms.plus.PlusClient;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Activity_main extends FragmentActivity  {
+
+    private LoginButton loginBtn;
+    private Button postImageBtn;
+    private Button updateStatusBtn;
+
+    private TextView userName;
+
+    private UiLifecycleHelper uiHelper;
+
+    private static final List<String> PERMISSIONS = Arrays.asList("publish_actions");
+
+    private static String message = "Sample status posted from android app";
+
+    private PlusClient mPlusClient;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+        uiHelper = new UiLifecycleHelper(this, statusCallback);
+        uiHelper.onCreate(savedInstanceState);
+
+
+       // mPlusClient = new PlusClient.Builder(this, this, this)
+       //         .setVisibleActivities("http://schemas.google.com/AddActivity", "http://schemas.google.com/BuyActivity")
+       //         .build();
+
+
+
+        setContentView(R.layout.activity_main);
+
+        userName = (TextView) findViewById(R.id.user_name);
+        loginBtn = (LoginButton) findViewById(R.id.fb_login_button);
+        loginBtn.setUserInfoChangedCallback(new LoginButton.UserInfoChangedCallback() {
+            @Override
+            public void onUserInfoFetched(GraphUser user) {
+                if (user != null) {
+                    userName.setText("Hello, " + user.getName());
+                } else {
+                    userName.setText("You are not logged");
+                }
+            }
+        });
+
+        postImageBtn = (Button) findViewById(R.id.post_image);
+        postImageBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Log.d("ImageButton", "pressed");
+                postImage();
+            }
+        });
+
+        updateStatusBtn = (Button) findViewById(R.id.update_status);
+        updateStatusBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.d("StatusButton","pressed");
+                postStatusMessage();
+            }
+        });
+
+        buttonsEnabled(false);
+    }
+
+    private Session.StatusCallback statusCallback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState state,
+                         Exception exception) {
+            if (state.isOpened()) {
+                buttonsEnabled(true);
+                Log.d("FacebookSampleActivity", "Facebook session opened");
+            } else if (state.isClosed()) {
+                buttonsEnabled(false);
+                Log.d("FacebookSampleActivity", "Facebook session closed");
+            }
+        }
+    };
+
+    public void buttonsEnabled(boolean isEnabled) {
+        postImageBtn.setEnabled(isEnabled);
+        updateStatusBtn.setEnabled(isEnabled);
+    }
+
+    public void postImage() {
+        if (checkPermissions()) {
+            Bitmap img = BitmapFactory.decodeResource(getResources(),
+                    R.drawable.ic_launcher);
+            Request uploadRequest = Request.newUploadPhotoRequest(
+                    Session.getActiveSession(), img, new Request.Callback() {
+                        @Override
+                        public void onCompleted(Response response) {
+                            Toast.makeText(Activity_main.this,
+                                    "Photo uploaded successfully",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+            );
+            uploadRequest.executeAsync();
+        } else {
+            requestPermissions();
+        }
+    }
+
+    public void postStatusMessage() {
+        if (checkPermissions()) {
+            Request request = Request.newStatusUpdateRequest(
+                    Session.getActiveSession(), message,
+                    new Request.Callback() {
+                        @Override
+                        public void onCompleted(Response response) {
+                            if (response.getError() == null)
+                                Toast.makeText(getApplication(),
+                                        "Status updated successfully",
+                                        Toast.LENGTH_LONG).show();
+                        }
+                    });
+            request.executeAsync();
+        } else {
+            requestPermissions();
+        }
+    }
+
+    public boolean checkPermissions() {
+        Session s = Session.getActiveSession();
+        if (s != null) {
+            return s.getPermissions().contains("publish_actions");
+        } else
+            return false;
+    }
+
+    public void requestPermissions() {
+        Session s = Session.getActiveSession();
+        if (s != null)
+            s.requestNewPublishPermissions(new Session.NewPermissionsRequest(
+                    this, PERMISSIONS));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        uiHelper.onResume();
+        buttonsEnabled(Session.getActiveSession().isOpened());
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uiHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedState) {
+        super.onSaveInstanceState(savedState);
+        uiHelper.onSaveInstanceState(savedState);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
